8be4cf69cc1d154af2732d4dc16d2bd13a17e970
Svilen Vassilev <svilen@rubystudio.net>
2013-04-08 15:53:12 +0300

    Update rails_admin_map_field.rb

--- lib/rails_admin_map_field.rb
+++ lib/rails_admin_map_field.rb
@@ -1,5 +1,6 @@
 require 'rails_admin'
 require 'rails_admin_map_field/rails_admin/config/fields/types/map'
+require 'rails_admin_map_field/rails_admin/config/fields/types/showmap'
 load 'rails_admin_map_field/engine.rb'
 
 # load 'rails_admin_map_field/factory.rb'
